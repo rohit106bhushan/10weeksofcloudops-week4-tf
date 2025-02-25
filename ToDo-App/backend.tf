677ccb4e3e362f4ddbd29ee9631630e18752db43
Rohit Bhushan <58414896+rohit106bhushan@users.noreply.github.com>
2025-02-25 14:53:37 +0530

    Update backend.tf
    added s3

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "10weeksofcloudops-week4"
+    bucket = "10weeksofcloudops-week4-rohit "
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
     dynamodb_table = "10weeksofcloudops-dynamo"
